1c7d885d6d900ea3e29e3794b2f6f81dfad1a662
Mladen Brankovic <root@brankovic.dev>
2020-03-17 10:51:48 +0100

    Tried using Flame.images

--- lib/views/game.dart
+++ lib/views/game.dart
@@ -1,6 +1,10 @@
+import 'dart:ui';
+
+import 'package:flame/components/component.dart';
+import 'package:flame/flame.dart';
 import 'package:flame/game.dart';
 import 'package:flame/sprite.dart';
-import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 
 class Game extends BaseGame {
   Size screenSize;
@@ -16,12 +20,24 @@ class Game extends BaseGame {
 
   void render(Canvas canvas) {
     Rect background = Rect.fromLTWH(0, 0, screenSize.width, screenSize.height);
-    Paint backgroundPaint = Paint()..color = Color(0xFF333333);
+    Paint backgroundPaint = Paint()..color = Color(0xFF555555);
 
     canvas.drawRect(background, backgroundPaint);
 
-    Sprite sprite = new Sprite('sprites/snakes/default/circle.png');
-    sprite.render(canvas);
+    //  sprite = new // ('sprites/snakes/default/circle.png');
+    // SpriteComponent player = SpriteComponent.fromSprite(100, 100, sprite);
+
+    // player.x = screenSize.width / 2 - player.width / 2;
+    // player.y = screenSize.height / 2 - player.height / 2;
+
+    // player.render(canvas);
+
+    Flame.images.load('sprites/snakes/default/circle.png').then((Image image) {
+      Paint paint = Paint()..color = Color(0xFFFFFFFF);
+      Rect rect = Rect.fromLTWH(0, 0, image.width.toDouble(), image.height.toDouble());
+
+      canvas.drawImageRect(image, rect, rect, paint);
+    });
   }
 
   void resize(Size size) {
