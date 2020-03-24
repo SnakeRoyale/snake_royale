bb2083223d46f348e3ccf017fdcc3398d127d999
Mladen Brankovic <root@brankovic.dev>
2020-03-24 09:41:17 +0100

    Refactor Snake and SnakeBody

--- lib/components/snake.dart
+++ lib/components/snake.dart
@@ -1,72 +1,47 @@
 import 'dart:ffi';
 import 'dart:ui';
 
-import 'package:flame/components/component.dart';
-import 'package:flame/flame.dart';
-import 'package:flame/sprite.dart';
+import 'package:snake_royale/components/snake_body.dart';
 import 'package:snake_royale/interfaces/movable.dart';
 
-class Snake implements Movable{
+class Snake implements Movable {
   String name;
   String color;
   Uint32 mass;
-  List<SpriteComponent> sprites = new List();
 
+  // List<SpriteComponent> sprites = new List();
+  List<SnakeBody> bodyParts = new List();
 
-  void update(double t){
-
-  }
-
-  Snake(int length){
+  Snake(int length) {
     _createHead(new Offset(150, 250));
     _createBody(length);
   }
 
-  void _createHead(Offset offset){
-    var circle = Flame.images.loadedFiles['sprites/snakes/default/head.png'];
-    Sprite sprite = Sprite.fromImage(circle);
-    double size = circle.height.toDouble();
-    var headSprite = SpriteComponent.fromSprite(size, size, sprite); // width, height, sprite
-    headSprite.x = offset.dx; // 0 by default
-    headSprite.y = offset.dy; // 0 by default
-    headSprite.angle = 0; // 0 by default
-    this.sprites.add(headSprite);
+  void update(double t) {}
+
+  void _createHead(Offset offset) {
+    var head = new SnakeBody(offset, isHead: true);
+    this.bodyParts.add(head);
   }
 
-  void _drawBody(Canvas canvas, Offset offset){
+  void _drawBody(Canvas canvas, Offset offset) {}
 
-  }
-  void render(Canvas canvas){
-      for(var component in this.sprites){
-        component.render(canvas);
-      }
+  void render(Canvas canvas) {
+    for (var bodyPart in this.bodyParts) {
+      bodyPart.render(canvas);
+    }
   }
 
-  Future<void> move(Offset offset) async{
-    for(var component in this.sprites){
-      component.x += offset.dx;
-      component.y += offset.dy;
+  Future<void> move(Offset offset) async {
+    for (var bodyPart in this.bodyParts) {
+      bodyPart.move(offset);
     }
   }
 
   void _createBody(int length) {
-    var circle = Flame.images.loadedFiles['sprites/snakes/default/circle.png'];
-
-    for(int i = 0; i < length; i ++){
-      Sprite sprite = Sprite.fromImage(circle);
-      double size = circle.height.toDouble();
-      var bodySprite = SpriteComponent.fromSprite(size, size, sprite); // width, height, sprite
-      bodySprite.x = 0; // 0 by default
-      bodySprite.y = 30; // 0 by default
-
-      if(i == 0){
-        bodySprite.x = -35;
-        bodySprite.y = 20; // 0 by default
-
-      }
-      bodySprite.angle = 0; // 0 by default
-      this.sprites.add(bodySprite);
+    for (int i = 0; i < length; i++) {
+      var bodyPart = new SnakeBody(new Offset(0, 30));
+      this.bodyParts.add(bodyPart);
     }
-
   }
 }
--- lib/components/snake_body.dart
+++ lib/components/snake_body.dart
@@ -6,66 +6,39 @@ import 'package:flame/flame.dart';
 import 'package:flame/sprite.dart';
 import 'package:snake_royale/interfaces/movable.dart';
 
-class SnakeBody implements Movable{
+class SnakeBody implements Movable {
   String name;
   String color;
   Uint32 mass;
-  List<SpriteComponent> sprites = new List();
+  SpriteComponent sprite;
 
-  void update(double t){
-
+  SnakeBody(Offset position, {bool isHead = false}) {
+    _draw(position, isHead);
   }
 
-  SnakeBody(int length){
-    _createHead(new Offset(150, 250));
-    _createBody(length);
+  void update(double t) {}
+
+  void _drawBody(Canvas canvas, Offset offset) {}
+
+  void render(Canvas canvas) {
+    this.sprite.render(canvas);
   }
 
-  void _createHead(Offset offset){
-    var circle = Flame.images.loadedFiles['sprites/snakes/default/head.png'];
-    Sprite sprite = Sprite.fromImage(circle);
+  void move(Offset offset) {
+    this.sprite.x += offset.dx;
+    this.sprite.y += offset.dy;
+  }
+
+  void _draw(Offset position, bool isHead) {
+    var circle = Flame.images.loadedFiles[isHead
+        ? 'sprites/snakes/default/head.png'
+        : 'sprites/snakes/default/circle.png'];
+    var sprite = Sprite.fromImage(circle);
     double size = circle.height.toDouble();
-    var headSprite = SpriteComponent.fromSprite(size, size, sprite); // width, height, sprite
-    headSprite.x = offset.dx; // 0 by default
-    headSprite.y = offset.dy; // 0 by default
-    headSprite.angle = 0; // 0 by default
-    this.sprites.add(headSprite);
-  }
-
-  void _drawBody(Canvas canvas, Offset offset){
-
-  }
-  void render(Canvas canvas){
-    for(var component in this.sprites){
-      component.render(canvas);
-    }
-  }
-
-  void move(Offset offset){
-    for(var component in this.sprites){
-      component.x += offset.dx;
-      component.y += offset.dy;
-    }
-  }
-
-  void _createBody(int length) {
-    var circle = Flame.images.loadedFiles['sprites/snakes/default/circle.png'];
-
-    for(int i = 0; i < length; i ++){
-      Sprite sprite = Sprite.fromImage(circle);
-      double size = circle.height.toDouble();
-      var bodySprite = SpriteComponent.fromSprite(size, size, sprite); // width, height, sprite
-      bodySprite.x = 0; // 0 by default
-      bodySprite.y = 30; // 0 by default
-
-      if(i == 0){
-        bodySprite.x = -35;
-        bodySprite.y = 20; // 0 by default
-
-      }
-      bodySprite.angle = 0; // 0 by default
-      this.sprites.add(bodySprite);
-    }
-
+    this.sprite =
+        SpriteComponent.fromSprite(size, size, sprite); // width, height, sprite
+    this.sprite.x = position.dx; // 0 by default
+    this.sprite.y = position.dy; // 0 by default
+    this.sprite.angle = 0; // 0 by default
   }
 }
