1b2d14a8e06fc9c291affb5a70376a3ed10abd00
Dominic Järmann <dominic.jaermann@icloud.com>
2020-05-19 09:32:38 +0200

    Show highscore label

--- lib/game/screens/highscore_screen.dart
+++ lib/game/screens/highscore_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:snaake/game/widgets/bezier_clipper.dart';
+import 'package:snaake/game/widgets/center_horizontal.dart';
 
 class HighscoreScreen extends StatefulWidget {
   final double height;
@@ -12,10 +14,11 @@ class HighscoreScreen extends StatefulWidget {
   _HighscoreScreenState createState() => _HighscoreScreenState();
 }
 
-class _HighscoreScreenState extends State<HighscoreScreen> with TickerProviderStateMixin {
+class _HighscoreScreenState extends State<HighscoreScreen>
+    with TickerProviderStateMixin {
   AnimationController _controller;
   Animation<double> animation;
-  final double startingHeight  =20.0;
+  final double startingHeight = 20.0;
 
   @override
   void initState() {
@@ -25,31 +28,46 @@ class _HighscoreScreenState extends State<HighscoreScreen> with TickerProviderSt
     _controller =
         AnimationController(vsync: this, duration: Duration(seconds: 1));
     animation = Tween<double>(
-      begin: 50.0,
+      begin: 0.0,
       end: 280.0,
     ).animate(
       CurvedAnimation(
         parent: _controller,
         curve: Interval(
-          0.125, 0.450,
+          0.125,
+          0.450,
           curve: Curves.ease,
         ),
       ),
     );
     _controller.forward(from: 0.0);
-
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: AnimatedBuilder(
         builder: (context, anim) {
-          return ClipPath(
-            clipper: BezierClipper(animationValue: animation.value),
-            child: Container(
-              height: animation.value,
-              color: Colors.blueAccent,
-            ),
+          return Stack(
+            children: <Widget>[
+              ClipPath(
+                clipper: BezierClipper(animationValue: animation.value),
+                child: Container(
+                  height: animation.value,
+                  color: Colors.blueAccent,
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(vertical: 100),
+                child: CenterHorizontal(
+                 Text(
+                    'Highscore',
+                    textAlign: TextAlign.center,
+                   style: TextStyle(color: Colors.white, fontSize: 35),
+                  ),
+                ),
+              )
+            ],
           );
         },
         animation: _controller,
