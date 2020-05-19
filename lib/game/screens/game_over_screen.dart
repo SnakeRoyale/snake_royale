8859ffbde118cda822ace5f3509233682e198180
Mladen Brankovic <root@brankovic.dev>
2020-05-19 10:11:59 +0200

    Add reference to addScore

--- lib/game/screens/game_over_screen.dart
+++ lib/game/screens/game_over_screen.dart
@@ -1,21 +1,27 @@
 import 'package:flutter/cupertino.dart';
+import 'package:snaake/game/service/highscore_service.dart';
+import 'package:snaake/game/service/score_model.dart';
 
 class GameOverScreen extends StatefulWidget {
   bool won;
+  int score;
 
-  GameOverScreen({this.won, Key key}) : super(key: key);
+  GameOverScreen({this.won, this.score, Key key}) : super(key: key);
 
   @override
-  _GameOverScreenState createState() => _GameOverScreenState(won: won);
+  _GameOverScreenState createState() =>
+      _GameOverScreenState(won: won, score: score);
 }
 
 class _GameOverScreenState extends State<GameOverScreen> {
   bool won;
+  int score;
 
-  _GameOverScreenState({this.won});
+  _GameOverScreenState({this.won, this.score});
 
   @override
   Widget build(BuildContext context) {
+    HightscoreService().addScore(ScoreModel(score, DateTime.now()));
     return Center(
       child: Text('yikes'),
     );
